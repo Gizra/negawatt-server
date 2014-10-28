c563ac5fa2c6706cbdb731addbcb84cda702d113
Amitaibu <amitai@gizra.com>
2014-10-28 16:10:29 +0200

    Add pantheon install.

---
+++ pantheon-install.example.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+PANTHEON_DIR="/var/www/pantheon-negawatt"
+
+cd jekyll
+npm install
+bower install
+grunt deploy
+cd ..
+
+rm -rf $PANTHEON_DIR/profiles/negawatt
+cp -R pipeline $PANTHEON_DIR/profiles/negawatt
+
+cd $PANTHEON_DIR
+git add .
+git commit -am "Site update"
+git push
+
+cd -
+# Excute Pantheon install outside of the Pantheon Dir.
+# Make sure you have the alias setup (use `drush sa` too see the aliases).
+
+drush @pantheon.negawatt.dev sql-drop -y --strict=0
+drush @pantheon.negawatt.dev si -y negawatt --account-pass=admin
+# Enable Pantheon's Apache Solr integration.
+drush @pantheon.negawatt.dev en pantheon_apachesolr -y --strict=0
+drush @pantheon.negawatt.dev en negawatt_migrate -y --strict=0
+drush @pantheon.negawatt.dev mi --all --user=1 --strict=0
+drush @pantheon.negawatt.dev uli
