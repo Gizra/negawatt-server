edd52728fb7bef0992cdcbb01b26a51bf9568217
Shahar <shahard@arava-ect.com>
2014-12-23 16:26:44 +0200

    Built negawattclient from commit 6817493 on branch master

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('Config', {backend:'http://server.com/api',frontend:'http://server.com'})
+.constant('Config', {backend:'http://dev-negawatt.pantheon.io',frontend:'http://gizra.github.io/negawatt-server'})
 
 ;
