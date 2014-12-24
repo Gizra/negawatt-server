2dae2b81eedd60180c0d558249a818eca9073eb2
Carlos Mantilla <cmantilla@aliongroo.com>
2014-12-24 15:18:52 +0200

    create controller to handle map events.

--- client/app/scripts/controllers/dashboard.js
+++ client/app/scripts/controllers/dashboard.js
@@ -57,26 +57,5 @@ angular.module('negawattClientApp')
       return (category.meters > 0) ? true : false;
     };
 
-    var openedPopup = null;
-
-    // Hover above marker in the Map -  open tooltip
-    $scope.$on("leafletDirectiveMarker.mouseover", function(event, args) {
-      var leafletId = args.markerName;
-      // If this popup is already opened, do nothing
-      if ($stateParams.openedPopup == leafletId) {
-        return;
-      }
-      // Open tooltip popup window
-      $stateParams.openedPopup = leafletId;
-      var leaflet = args.leafletEvent.target;
-      leaflet.openPopup();
-    });
-
-    // Hover out of marker in the Map - close tooltip
-    $scope.$on("leafletDirectiveMarker.mouseout", function(event, args) {
-      var leaflet = args.leafletEvent.target;
-      leaflet.closePopup();
-      $stateParams.openedPopup = null;
-    });
 
   });
