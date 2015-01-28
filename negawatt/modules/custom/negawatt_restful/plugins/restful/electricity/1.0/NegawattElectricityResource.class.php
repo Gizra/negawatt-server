53e14a61f59e70debac7852d3fdbd1e6ee51000e
Shahar <shahard@arava-ect.com>
2015-01-28 16:41:08 +0200

    Fixes
    Reorder grouping
    Add human readable date-time to api/electricity
    Remove totals at the end (don't use special formatter)

--- negawatt/modules/custom/negawatt_restful/plugins/restful/electricity/1.0/NegawattElectricityResource.class.php
+++ negawatt/modules/custom/negawatt_restful/plugins/restful/electricity/1.0/NegawattElectricityResource.class.php
@@ -19,6 +19,10 @@ class NegawattElectricityResource extends \RestfulDataProviderDbQuery implements
       'property' => 'timestamp'
     );
 
+    $public_fields['datatime'] = array(
+      'property' => 'datetime'
+    );
+
     $public_fields['rate_type'] = array(
       'property' => 'rate_type'
     );
@@ -73,13 +77,18 @@ class NegawattElectricityResource extends \RestfulDataProviderDbQuery implements
     $query->leftJoin($table_name, 'cat', 'cat.entity_id=meter_nid');
     $query->addField('cat', 'field_meter_category_target_id', 'meter_category');
 
+    // Add human readable date-time field
+    $query->addExpression('from_unixtime(timestamp) ', 'datetime');
+
+    // Make summary fields
     $query->addExpression('SUM(sum_kwh)', 'sum_kwh');
     $query->addExpression('MIN(min_power_factor)', 'min_power_factor');
     $query->addExpression('AVG(avg_power)', 'avg_power');
 
-    $query->groupBy('type');
-    $query->groupBy('rate_type');
+    // Set grouping.
     $query->groupBy('timestamp');
+    $query->groupBy('rate_type');
+    $query->groupBy('type');
 
     return $query;
   }
