50dbb8b0fe000ae31576bcc5784e9afd4ed16af9
Shahar <shahard@arava-ect.com>
2015-03-18 15:17:17 +0200

    Request only meters with electricity data

--- client/app/scripts/services/meter.js
+++ client/app/scripts/services/meter.js
@@ -60,8 +60,12 @@ angular.module('negawattClientApp')
       var url;
       pageNumber = pageNumber || 1;
 
-      // Define endpoint.
-      url = Config.backend + '/api/meters?filter[account]=' + accountId + '&page=' + pageNumber;
+      // Define endpoint with filters.
+      // Get only meters that has electricity data.
+      url = Config.backend + '/api/meters?'
+        + 'filter[has_electricity]=1'
+        + '&filter[account]=' + accountId
+        + '&page=' + pageNumber;
 
       $http({
         method: 'GET',
