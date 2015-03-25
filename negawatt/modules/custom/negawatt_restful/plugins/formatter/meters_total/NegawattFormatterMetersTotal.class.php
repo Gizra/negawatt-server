a2a4f4039d1ffcd66de4b2547d5e0b0aa641a335
Shahar <shahard@arava-ect.com>
2015-03-25 10:53:31 +0200

    Fix totals section for all meters requests.

--- negawatt/modules/custom/negawatt_restful/plugins/formatter/meters_total/NegawattFormatterMetersTotal.class.php
+++ negawatt/modules/custom/negawatt_restful/plugins/formatter/meters_total/NegawattFormatterMetersTotal.class.php
@@ -3,28 +3,33 @@
  * @file
  * Contains NegawattFormatterMetersTotal.
  */
-class NegawattFormatterMetersTotal extends \RestfulFormatterHalJson {
+class NegawattFormatterMetersTotal extends \RestfulFormatterJson {
   /**
    * {@inheritdoc}
+   *
+   * Add 'total' section to meters' output.
    */
   public function prepare(array $data) {
-// If we're returning an error then set the content type to
-// 'application/problem+json; charset=utf-8'.
+    // If we're returning an error then set the content type to
+    // 'application/problem+json; charset=utf-8'.
     if (!empty($data['status']) && floor($data['status'] / 100) != 2) {
       $this->contentType = 'application/problem+json; charset=utf-8';
       return $data;
     }
-    // Here we get the data after calling the backend storage for the resources.
+
+    // Let parent formatter prepare the output.
     $output = parent::prepare($data);
 
-    // Calculate totals for meters.
-    $min_ts = $data[0]->electricity_time_interval->min;
-    $max_ts = $data[0]->electricity_time_interval->max;
+    // Calculate total min/max timestamps for all the meters.
+    $min_ts = $data[0]['electricity_time_interval']->min;
+    $max_ts = $data[0]['electricity_time_interval']->max;
 
     foreach ($data as $row) {
-      $min_ts = min($min_ts ,$row['electricity_time_interval']['min']);
-      $max_ts = max($max_ts ,$row['electricity_time_interval']['max']);
+      $min_ts = min($min_ts ,$row['electricity_time_interval']->min);
+      $max_ts = max($max_ts ,$row['electricity_time_interval']->max);
     }
+
+    // Add total section to touput.
     $output['total']['electricity_time_interval']['min'] = $min_ts;
     $output['total']['electricity_time_interval']['max'] = $max_ts;
 
