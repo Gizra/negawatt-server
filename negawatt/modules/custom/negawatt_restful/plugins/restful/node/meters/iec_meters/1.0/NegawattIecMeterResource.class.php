624e37e7b5aa0cf48e98fb51cceedcb49645ff06
Shahar <shahard@arava-ect.com>
2015-04-30 17:04:53 +0300

    Move set_meter_category() to iec.module

--- negawatt/modules/custom/negawatt_restful/plugins/restful/node/meters/iec_meters/1.0/NegawattIecMeterResource.class.php
+++ negawatt/modules/custom/negawatt_restful/plugins/restful/node/meters/iec_meters/1.0/NegawattIecMeterResource.class.php
@@ -48,49 +48,6 @@ class NegawattIecMeterResource extends \NegawattEntityMeterBase {
       return parent::updateEntity($id);
     }
     // New node.
-    $return = parent::createEntity();
-
-
-    // Set meter category.
-
-    $new_meter_id = $return[0]['id'];
-    $wrapper = entity_metadata_wrapper('node', $new_meter_id);
-
-    // Get vocabulary according to meter account (organic-group).
-    $account = $wrapper->{OG_AUDIENCE_FIELD}->value();
-    $group_id = $account->nid;
-    $arr_vocabularies = og_vocab_relation_get_by_group('node', $group_id);
-    $vocabulary_id = $arr_vocabularies[0]->vid;
-    $vocabulary = taxonomy_vocabulary_load($vocabulary_id);
-
-    // Try match meter description with any of the categories' match-strings
-    $category = 'אחר';
-    $taxonomy_tree = taxonomy_get_tree($vocabulary_id);
-    $search_string = $wrapper->field_place_address->value() . ' ' . $wrapper->field_place_description->value();
-    foreach ($taxonomy_tree as $term) {
-      $tax_wrapper = entity_metadata_wrapper('taxonomy_term', $term->tid);
-      $match_strings = $tax_wrapper->field_match_strings->value();
-      $match_strings = explode('|', $match_strings);
-      foreach ($match_strings as $match_str) {
-        if (strpos($search_string, $match_str) !== FALSE) {
-          $category = $tax_wrapper->name->value();
-          // Leave both loops.
-          break 2;
-        }
-      }
-    }
-
-    // Set meter-category.
-    $arr_terms = taxonomy_get_term_by_name($category, $vocabulary->machine_name);
-    $term_ids = array_keys($arr_terms);
-    $term_id = $term_ids[0];
-
-    $wrapper->{OG_VOCAB_FIELD}->set(array($term_id));
-    $wrapper->save();
-
-    // Update returned node with new category.
-    $return = array($this->viewEntity($new_meter_id));
-
-    return $return;
+    return parent::createEntity();
   }
 }
