e722e4dbefa0873a0a6c5873fd8b3747bb0454fb
Carlos Mantilla <cmantilla@aliongroo.com>
2015-05-20 15:19:50 +0300

    feat: add lazy load to electricity data.

--- client/app/scripts/directives/chart-electricity-usage.directive.js
+++ client/app/scripts/directives/chart-electricity-usage.directive.js
@@ -5,11 +5,13 @@ angular.module('negawattDirectives', [])
     return {
       restrict: 'EA',
       templateUrl: 'scripts/directives/chart-electricity-usage.directive.html',
-      controller: function(Chart, FilterFactory, $state, $stateParams, $timeout, $urlRouter, $location, $filter) {
+      controller: function(Chart, FilterFactory, $state, $stateParams, $timeout, $urlRouter, $location, $filter, $scope) {
         var ctrlChart = this;
 
-        // Define default chart data.
-        ctrlChart.data = $filter('toChartDataset')(ctrlChart.electricity);
+        $scope.$watch('ctrlChart.electricity', function(current) {
+          // Define default chart data.
+          ctrlChart.data = $filter('toChartDataset')(ctrlChart.electricity);
+        });
 
         // Update data object
         // Get chart frequencies. (Tabs the period of time)
@@ -26,21 +28,21 @@ angular.module('negawattDirectives', [])
           $state.refreshUrlWith({chartFreq: +type});
           return type;
 
-          var params = {};
-
-          // Change of frequency, request the new electricity data on the selected frequency and prepare
-          // the query string to be updated..
-          if ($stateParams.chartFreq !== type) {
-            $stateParams.chartFreq = type;
-            // Delete properties in the case we change the frecuency (day, month, year).
-            if (chartUpdated) {
-              delete $stateParams['chartNextPeriod'];
-              delete $stateParams['chartPreviousPeriod'];
-            }
-          }
-
-          // Udpate chart.
-          updateUsageChart(params);
+          //var params = {};
+          //
+          //// Change of frequency, request the new electricity data on the selected frequency and prepare
+          //// the query string to be updated..
+          //if ($stateParams.chartFreq !== type) {
+          //  $stateParams.chartFreq = type;
+          //  // Delete properties in the case we change the frecuency (day, month, year).
+          //  if (chartUpdated) {
+          //    delete $stateParams['chartNextPeriod'];
+          //    delete $stateParams['chartPreviousPeriod'];
+          //  }
+          //}
+          //
+          //// Udpate chart.
+          //updateUsageChart(params);
         }
 
         /**
@@ -68,35 +70,35 @@ angular.module('negawattDirectives', [])
          * @param period
          *   Period of time expresed in timestamp, used to request specific electricity data.
          */
-        function updateUsageChart(params, period) {
-
-          // In case the state activation is enter via URL.
-          if (angular.isUndefined(period) && !$state.transition && (angular.isDefined($stateParams.chartNextPeriod) || angular.isDefined($stateParams.chartNextPeriod) )) {
-            period = {
-              next: $stateParams.chartNextPeriod,
-              previous: $stateParams.chartPreviousPeriod
-            };
-            angular.extend(params, {
-              chartNextPeriod: $stateParams.chartNextPeriod,
-              chartPreviousPeriod: $stateParams.chartPreviousPeriod
-            });
-          }
-
-          // Load electricity data in the chart according the chart frequency.
-          //$scope.isLoading = true;
-
-          // Extend period with the maximum and minimum time, if a marker is selected.
-          if (angular.isDefined($stateParams.markerId)) {
-            period = angular.extend(period || {}, meters.list[$stateParams.markerId] && meters.list[$stateParams.markerId].electricity_time_interval);
-          }
-
-          // @TODO: Implement ChartUsage.render();
-          //ChartUsage.get(account.id, $stateParams, period).then(function(response) {
-          //  $scope.usageChartData = response;
-          //  $scope.isLoading = false;
-          //});
-
-        }
+        //function updateUsageChart(params, period) {
+        //
+        //  // In case the state activation is enter via URL.
+        //  if (angular.isUndefined(period) && !$state.transition && (angular.isDefined($stateParams.chartNextPeriod) || angular.isDefined($stateParams.chartNextPeriod) )) {
+        //    period = {
+        //      next: $stateParams.chartNextPeriod,
+        //      previous: $stateParams.chartPreviousPeriod
+        //    };
+        //    angular.extend(params, {
+        //      chartNextPeriod: $stateParams.chartNextPeriod,
+        //      chartPreviousPeriod: $stateParams.chartPreviousPeriod
+        //    });
+        //  }
+        //
+        //  // Load electricity data in the chart according the chart frequency.
+        //  //$scope.isLoading = true;
+        //
+        //  // Extend period with the maximum and minimum time, if a marker is selected.
+        //  if (angular.isDefined($stateParams.markerId)) {
+        //    period = angular.extend(period || {}, meters.list[$stateParams.markerId] && meters.list[$stateParams.markerId].electricity_time_interval);
+        //  }
+        //
+        //  // @TODO: Implement ChartUsage.render();
+        //  //ChartUsage.get(account.id, $stateParams, period).then(function(response) {
+        //  //  $scope.usageChartData = response;
+        //  //  $scope.isLoading = false;
+        //  //});
+        //
+        //}
       },
       controllerAs: 'ctrlChart',
       bindToController: true,
