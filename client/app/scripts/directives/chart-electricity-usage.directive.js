057e1f6fce5c0d128a47c0d40261cfb73cf2f41c
Carlos Mantilla <cmantilla@aliongroo.com>
2015-05-25 16:07:47 +0300

    refactor: show data in the the chart.

--- client/app/scripts/directives/chart-electricity-usage.directive.js
+++ client/app/scripts/directives/chart-electricity-usage.directive.js
@@ -29,34 +29,19 @@ angular.module('negawattDirectives', [])
          */
         ctrlChart.changeFrequency = function(type) {
           // Update the electricity filters.
-          updateElectricityFilters(type);
+          updateElectricityFilters({chartFreq: +type});
 
-          // Update with the actual data.
-          ctrlChart.data = $filter('toChartDataset')($filter('activeElectricityFilters')(ctrlChart.electricity));
-
-          // Refresh electricity data.
-          Electricity.refresh(FilterFactory.get('activeElectricityHash'));
-
-          //var params = {};
-          //
-          //// Change of frequency, request the new electricity data on the selected frequency and prepare
-          //// the query string to be updated..
-          //if ($stateParams.chartFreq !== type) {
-          //  $stateParams.chartFreq = type;
-          //  // Delete properties in the case we change the frecuency (day, month, year).
-          //  if (chartUpdated) {
-          //    delete $stateParams['chartNextPeriod'];
-          //    delete $stateParams['chartPreviousPeriod'];
-          //  }
-          //}
-          //
-          //// Udpate chart.
-          //updateUsageChart(params);
+          refreshChart();
         }
 
 
         ctrlChart.changePeriod = function(type) {
-          debugger;
+          var newPeriod = UsagePeriod.newPeriod(type);
+
+          // Update Electricity Filter.
+          updateElectricityFilters({chartNextPeriod: newPeriod.next, chartPreviousPeriod: newPeriod.previous});
+
+          refreshChart();
         }
 
         /**
@@ -79,54 +64,29 @@ angular.module('negawattDirectives', [])
         /**
          * Update the electricity filters in UI-Router and FilterFactory
          *
-         * @param type
-         *  The chart requency selected
+         * @param params
+         *  The new parameters to be updated on the filters.
          */
-        function updateElectricityFilters(type) {
+        function updateElectricityFilters(params) {
           // Update url with params updated.
-          angular.extend($stateParams, {chartFreq: +type});
+          angular.extend($stateParams, params);
           $state.refreshUrlWith($stateParams);
           // Refresh the electricity filters as active, and generate new hash.
           FilterFactory.set('electricity', $stateParams);
         }
 
         /**
-         * Search the data with the new chart frequency.
-         *
-         * @param params
-         *   Search params used to the query string the next time.
-         * @param period
-         *   Period of time expresed in timestamp, used to request specific electricity data.
+         * Update chart with cache data or clean an get the new data from
+         * electricity. Generally update data deom
          */
-        //function updateUsageChart(params, period) {
-        //
-        //  // In case the state activation is enter via URL.
-        //  if (angular.isUndefined(period) && !$state.transition && (angular.isDefined($stateParams.chartNextPeriod) || angular.isDefined($stateParams.chartNextPeriod) )) {
-        //    period = {
-        //      next: $stateParams.chartNextPeriod,
-        //      previous: $stateParams.chartPreviousPeriod
-        //    };
-        //    angular.extend(params, {
-        //      chartNextPeriod: $stateParams.chartNextPeriod,
-        //      chartPreviousPeriod: $stateParams.chartPreviousPeriod
-        //    });
-        //  }
-        //
-        //  // Load electricity data in the chart according the chart frequency.
-        //  //$scope.isLoading = true;
-        //
-        //  // Extend period with the maximum and minimum time, if a marker is selected.
-        //  if (angular.isDefined($stateParams.markerId)) {
-        //    period = angular.extend(period || {}, meters.list[$stateParams.markerId] && meters.list[$stateParams.markerId].electricity_time_interval);
-        //  }
-        //
-        //  // @TODO: Implement ChartUsage.render();
-        //  //ChartUsage.get(account.id, $stateParams, period).then(function(response) {
-        //  //  $scope.usageChartData = response;
-        //  //  $scope.isLoading = false;
-        //  //});
-        //
-        //}
+        function refreshChart() {
+          // Update with the actual data.
+          ctrlChart.data = $filter('toChartDataset')($filter('activeElectricityFilters')(ctrlChart.electricity));
+
+          // Refresh electricity data.
+          Electricity.refresh(FilterFactory.get('activeElectricityHash'));
+        }
+
       },
       controllerAs: 'ctrlChart',
       bindToController: true,
