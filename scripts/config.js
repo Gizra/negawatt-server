0ec7e92174baa3ad0b5ae35db2a17481c8f19d1b
Carlos Mantilla <cmantilla@aliongroo.com>
2015-06-16 11:12:02 +0300

    Built negawattclient from commit d2fd862 on branch master

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('Config', {backend:'http://dev-negawatt.pantheon.io',cname:'',debugUiRouter:false,openweather:{endpoint:null,apikey:'9a67a4889f54dd2a86858f692bfbe62e'}})
+.constant('Config', {backend:'http://dev-negawatt.pantheon.io',cname:'negawatt-dev.gizra.com',debugUiRouter:false})
 
 ;
