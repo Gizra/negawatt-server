cc315d5b97420025ffa70bd75de927aff1aa64a7
Shahar <shahard@arava-ect.com>
2015-07-15 17:12:12 +0300

    Squashed merge 691
    commit 40b193a541d96c250a6f09d580a7df16e11f78bd
    Author: Shahar <shahard@arava-ect.com>
    Date:   Tue Jul 14 15:29:59 2015 +0300
    
        Catch bad records before they produce bad values

--- negawatt/modules/custom/negawatt_normalizer/plugins/electricity_normalizer/modbus/ElectricityNormalizerModbus.class.php
+++ negawatt/modules/custom/negawatt_normalizer/plugins/electricity_normalizer/modbus/ElectricityNormalizerModbus.class.php
@@ -25,12 +25,20 @@ class ElectricityNormalizerModbus extends \ElectricityNormalizerBase {
     if (!$prev_record) {
       // No previous record, can't figure out kWhs...
       $processed_record->kwh = 0;
+      return $processed_record;
     }
-    else {
-      // Perv-record exists, calc kWhs delta.
-      $processed_record->kwh -= $prev_record->kwh;
+
+    // Catch bad records
+    if ($processed_record->kwh == 0 || $prev_record->kwh == 0) {
+      // kwh == 0 means a bad reading. Set kwh to 0 (best guess...).
+      // @fixme: maybe put a NULL value to denote a 'hole' in the data.
+      $processed_record->kwh = 0;
+      return $processed_record;
     }
+
+    // Perv-record exists, calc kWhs delta.
+    $processed_record->kwh -= $prev_record->kwh;
+
     return $processed_record;
   }
-
 }
