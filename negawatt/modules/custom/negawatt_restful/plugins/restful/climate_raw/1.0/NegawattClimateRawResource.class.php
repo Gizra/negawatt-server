764edc4ea69cb417e14fb8ef83f8a672d77676b5
Shahar <shahard@arava-ect.com>
2015-08-24 15:33:24 +0300

    Squashed merge 796
    commit a574744d67c346f2a14aff8fe4011717eccfd538
    Author: Shahar - MacBook <github.com@dolevim.org>
    Date:   Mon Aug 24 15:32:25 2015 +0300
    
        WIP: implement overrid getList()
    
    commit 1e39a3ff46e1e447481fa33833865d8ab2239041
    Author: Shahar <shahard@arava-ect.com>
    Date:   Mon Aug 24 15:24:33 2015 +0300
    
        Squashed merge master-0.7
    
        commit 6cfbe2e40dc814759e82a84ba492e505e91d61ca
        Author: Shahar <shahard@arava-ect.com>
        Date:   Mon Aug 24 15:10:16 2015 +0300
    
            Squashed merge 795
    
            commit 3f2c7a22b640c8e386c34a05c48071b0b55a9e64
            Author: Shahar <shahard@arava-ect.com>
            Date:   Mon Aug 24 15:09:09 2015 +0300
    
                Style
    
            commit 77a4540390a8b9d0a1e62adf81b9d068c8dc50c6
            Author: Shahar <shahard@arava-ect.com>
            Date:   Mon Aug 24 14:31:03 2015 +0300
    
                Handle creation of an new entity
    
            commit 6418d38800e8526781393a32bfb6b232eb833b2a
            Author: Shahar <shahard@arava-ect.com>
            Date:   Mon Aug 24 14:04:12 2015 +0300
    
                Add human-readable datetime
    
            commit d3b79e6defe954649b89209b1271d322a82f8117
            Author: Shahar <shahard@arava-ect.com>
            Date:   Mon Aug 24 09:59:27 2015 +0300
    
                Fix views
    
            commit f72c170a31715dd682934abb873efc74572c0474
            Author: Shahar <shahard@arava-ect.com>
            Date:   Mon Aug 24 09:54:42 2015 +0300
    
                Fix views
    
            commit ac0a54bbfa040dd01ddec80ed895eb6553dfc253
            Author: Shahar <shahard@arava-ect.com>
            Date:   Mon Aug 24 09:24:10 2015 +0300
    
                Fix occupancy definition
    
            commit 457a04db45acbb9bf68a5ac6b380231c875e0b5b
            Author: Shahar <shahard@arava-ect.com>
            Date:   Sun Aug 23 16:43:39 2015 +0300
    
                Fix view
    
            commit 18a22f3f8c79822b6b16538a7505fd5e1bd8337a
            Author: Shahar <shahard@arava-ect.com>
            Date:   Sun Aug 23 16:43:17 2015 +0300
    
                Add freq. filter to view
    
            commit b9dff2e7d942fde4bf8bda6ff57f24d4a69d3bcc
            Author: Shahar <shahard@arava-ect.com>
            Date:   Sun Aug 23 16:40:49 2015 +0300
    
                Add occupancy data storage and restful
    
            commit 4feae37cb378fe8903342eb29fbc086c1aa78e34
            Author: Shahar <shahard@arava-ect.com>
            Date:   Sun Aug 23 16:40:07 2015 +0300
    
                Misc
    
            commit 45b83f2a5a5ae43a34e203743888f2b9b362bf56
            Author: Shahar <shahard@arava-ect.com>
            Date:   Sun Aug 23 15:05:38 2015 +0300
    
                View fixes

--- negawatt/modules/custom/negawatt_restful/plugins/restful/climate_raw/1.0/NegawattClimateRawResource.class.php
+++ negawatt/modules/custom/negawatt_restful/plugins/restful/climate_raw/1.0/NegawattClimateRawResource.class.php
@@ -64,4 +64,52 @@ class NegawattClimateRawResource extends RestfulEntityBase {
     // New node.
     return parent::createEntity();
   }
+
+  /**
+   * Get a list of entities.
+   *
+   * @return array
+   *   Array of entities, as passed to RestfulEntityBase::viewEntity().
+   *
+   * @throws RestfulBadRequestException
+   */
+  public function getList() {
+    $request = $this->getRequest();
+
+    if (empty($request['key'])) {
+      // 'key' parameter was not given, run standard RESTful getList()
+      return parent::getList();
+    }
+
+    // 'key' parameter was given, create new climate entity.
+
+    // Make sure required parameters are given.
+    $required_parameters = array('account', 'timestamp', 'key');
+    foreach ($required_parameters as $required_parameter) {
+      if (empty($request[$required_parameter])) {
+        throw new \RestfulBadRequestException(format_string('Please supply \'@param\' parameter.', array('@param' => $required_parameter)));
+      }
+    }
+
+    // Read parameters.
+    $account = !empty($request['account']) ? $request['account'] : NULL;
+    $temperature = !empty($request['temperature']) ? $request['temperature'] : NULL;
+    $humidity = !empty($request['humidity']) ? $request['humidity'] : NULL;
+    $timestamp = !empty($request['time']) ? $request['time'] : NULL;
+    $key = !empty($request['key']) ? $request['key'] : NULL;
+
+    // Check key.
+    $key_base = 214357;
+    $calculated_key = ($temperature * $humidity * $timestamp) % $key_base;
+    if ($key != $calculated_key) {
+      throw new \RestfulBadRequestException('Wrong key parameter.');
+    }
+    // Key ok, remove it from the request since it's not a value parameter
+    unset($request['key']);
+
+    // Create the entity.
+    return $this->createEntity();
+  }
+
+
 }
