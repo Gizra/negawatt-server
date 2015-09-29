8fd8df845d5f6bbb9c0ba0dbea500d79ab0e3f14
Shahar - MacBook <github.com@dolevim.org>
2015-09-29 08:47:19 +0200

    Add interpolation for temperature data

--- client/app/scripts/directives/chart-detailed/chart-detailed.service.js
+++ client/app/scripts/directives/chart-detailed/chart-detailed.service.js
@@ -18,9 +18,9 @@ angular.module('negawattClientApp')
      * @returns {Promise}
      */
     function getElectricity(filters) {
-      //if (filters) {
-      //  FilterFactory.setElectricity(filters);
-      //}
+      if (filters) {
+        FilterFactory.setElectricity(filters);
+      }
       var electricity = Electricity.get(FilterFactory.get('activeElectricityHash'));
 
       return isUndefined(electricity) ? $q.promise : electricity;
@@ -55,10 +55,73 @@ angular.module('negawattClientApp')
      * @returns {Promise}
      */
     function getTemperature(filters) {
-      //FilterFactory.setTemperature(filters);
-      var temperature = Temperature.get(FilterFactory.get('activeTemperatureHash'));
+      if (filters) {
+        FilterFactory.setTemperature(filters);
+      }
+      var temperature = Temperature.get(FilterFactory.get('activeTemperatureHash'))
+        .then(function (data) {
+          return interpolateTemperature(data, filters);
+        });
 
       return isUndefined(temperature) ? $q.promise : temperature;
+    }
+
+    /**
+     * Interpoleate climate data if needed.
+     *
+     * Climate data might come in 3 hours intervals (IMS data). For day and weed charts, with only
+     * one data line (no meters selected, or one meter selected), broken line chart is used for
+     * TOUse colors, so interpolateNulls cannot be set to true. Hence, the missing data points
+     * must be interpolated
+     *
+     * @param data
+     *   The original data.
+     * @param filters
+     *   Filters of the query.
+     *
+     * @returns {array}
+     *   Data with interpolated data-points in the original 'holes'.
+     */
+    function interpolateTemperature(data, filters) {
+      // Interpolate only in day and week charts, when there's no meter selected or
+      // only one meter selected.
+      if (filters.chartFreq == 4 && (!filters.sel || filters.sel.split(',').length < 2)) {
+        // Hours frequency
+        var prevTimestamp = undefined,
+          prevValue = undefined;
+        var interpolatedData = [];
+        angular.forEach(data, function (item) {
+          var newTimestamp = item.timestamp_rounded,
+            newValue = item.avg_temp;
+          // Only from the second data point and on.
+          if (prevTimestamp) {
+            // Calculate the number of data points to interpolate in an hour (3600 secs.) interval.
+            var numInterpolatedPoints = (newTimestamp - prevTimestamp) / 3600 - 1,
+              dy = (newValue - prevValue) / (numInterpolatedPoints + 1);
+            // Add the new points.
+            for (var i = 0; i <numInterpolatedPoints; i++) {
+              // Advance the time and value for the interpolation.
+              prevTimestamp += 3600;
+              prevValue += dy;
+              // Add new data point.
+              interpolatedData.push({
+                timestamp: prevTimestamp,
+                timestamp_rounded: prevTimestamp,
+                avg_temp: prevValue
+              });
+            }
+          }
+          // Add the original data point too.
+          interpolatedData.push(item);
+          // Save the time and value for next iteration.
+          prevTimestamp = +newTimestamp;
+          prevValue = +newValue;
+        });
+        // Return the interpolated data.
+        return interpolatedData;
+      }
+      // No interpolation required, return the original data.
+      return data;
     }
 
   });
