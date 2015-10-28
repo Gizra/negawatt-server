74c926dc533d34148f6a803bc5fff1c009b6a823
Shahar <shahard@arava-ect.com>
2015-10-28 12:00:40 +0200

    Back to mapbox tiles in map.

--- client/app/scripts/services/map.js
+++ client/app/scripts/services/map.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('negawattClientApp')
-  .service('Map', function ($rootScope, $log, leafletData, leafletHelpers) {
+  .service('Map', function ($rootScope, $log, leafletData, leafletHelpers, Config) {
     var self = this;
 
     // Initial center point.
@@ -34,7 +34,8 @@ angular.module('negawattClientApp')
      */
     this.getConfig = function() {
       return {
-        tileLayer: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        tileLayer: 'http://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
+        tileLayerOptions: Config.tileLayerOptions,
         zoomControlPosition: 'bottomleft',
         minZoom: 12,
         maxZoom:19,
