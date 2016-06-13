8ffd539e88b940345154313898af02d3ad09d535
Brice Lenfant <brice@gizra.com>
2016-06-13 13:30:27 +0300

    Built negawattclient from commit 31ab38c on branch master

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('Config', {backend:'http://dev-negawatt.pantheon.io',cname:'negawatt-dev.gizra.com',debugUiRouter:false,openweather:{endpoint:null,apikey:'9a67a4889f54dd2a86858f692bfbe62e'},tileLayerOptions:{id:'niryariv.o0okn67c',accessToken:'pk.eyJ1IjoibmlyeWFyaXYiLCJhIjoiQjdJeWdqZyJ9.pZOwn6stABGoptmp0DH1wg'}})
+.constant('Config', {backend:'http://live-negawatt.pantheonsite.io/',debugUiRouter:false,openweather:{endpoint:null,apikey:'5znrzehcfsa35jarjywbsy9dymvnmxzq'}})
 
 ;
